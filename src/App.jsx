39d64fc18d63fac8695289c5162e2b035ca03962
agustincoda <codaagustin@gmail.com>
2024-06-23 23:49:02 -0300

    Agregando implementaciones basicas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the ultimate Dragonball Store!</h1>
+        <p style={{color:"orange"}}>Goku!</p>
+        <img src='../public/gokuSHFiguart.jpg' alt='Goku' />
+      </div>
+    </>
+  );
+}
+
+export default App;
